29b22b86cab397a147891d420685cb273903254f
bogdanlozhkin <bogdanlozhkin@gmail.com>
2020-12-11 23:59:05 +0000

    fix spacings

--- src/exercicio1/Exercicio2_Main.java
+++ src/exercicio1/Exercicio2_Main.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Exercicio2_Main {
-
     public static void main(String[] args) {
         ArrayList<String> pess = new ArrayList<String>();
         int option;
